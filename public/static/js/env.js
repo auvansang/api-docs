31c6e66c956b1252df3392dccd8e179ce6d26acc
Sang Au <auvansang@outlook.com>
2020-11-05 22:20:09 +0700

    Update packages

--- public/static/js/env.js
+++
@@ -1,7 +0,0 @@
-window.ENVIRONMENT = {
-  CLIENT_ID: 'api-docs',
-  CLIENT_SECRET: '',
-  REALM: '',
-  SCOPES: 'aaa bbb ccc',
-  DOCS: [{ url: 'http://113.161.58.33:10200/loan-application-api/swagger/v1/swagger.json', name: 'Loan', version: 1 }],
-};
